3b56af658d606b7558c65ea0c0873825b1e38a73
Alphacode18 <skharban@purdue.edu>
2020-07-16 16:24:52 +0530

    Shortened URL endpoint tested and implemented

--- app.js
+++ app.js
@@ -5,4 +5,17 @@ const userInput = promptSync("Enter The Url You Want Shortened:  ");
 
 const url = 'https://api.shrtco.de/v2/shorten?url=' + userInput;
 
-console.log(url);
+const request = unirest('GET', url);
+
+request.headers({
+	"accept": "application/json",
+});
+
+request.end((response) => {
+    if (!response) {
+        console.log('Something Went Wrong');
+    }
+    console.log('\n');
+    console.log(`Shortened Url: ${response.body.result.full_short_link}`);
+})
+
